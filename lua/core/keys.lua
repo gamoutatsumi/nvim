1a2f6295349e04aee6d3eb043e8ee23ec324f3a3
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2020-12-08 15:50:04 +0900

    add files

---
+++ lua/core/keys.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader = " "
+
+local api = vim.api
+
+api.nvim_set_keymap( 'n', 'x', '"_x', {noremap = true} )
+api.nvim_set_keymap( 'n', 's', '"_s', {noremap = true} )
+api.nvim_set_keymap( 'v', 'x', '"_x', {noremap = true} )
+api.nvim_set_keymap( 'v', 's', '"_s', {noremap = true} )
+api.nvim_set_keymap( 'n', '<CR>', ':<C-u>call append(".", "")<CR>', {noremap = true, silent = true} )
+api.nvim_set_keymap( 'n', '<Tab>', ':bnext<CR>', {noremap = true, silent = true} )
+api.nvim_set_keymap( 'n', '<S-Tab>', ':bprevious<CR>', {noremap = true, silent = true} )
+api.nvim_set_keymap( 'n', 'j', 'gj', {noremap = true} )
+api.nvim_set_keymap( 'n', 'k', 'gk', {noremap = true} )
+api.nvim_set_keymap( 'n', 'i', 'len(getline(".")) ? "i" : "cc"', {noremap = true, expr = true} )
+api.nvim_set_keymap( 'n', 'A', 'len(getline(".")) ? "A" : "cc"', {noremap = true, expr = true} )
+api.nvim_set_keymap( 'n', 'Y', 'y$', {noremap = true} )
---
+++ lua/core/options.lua
@@ -0,0 +1,46 @@
+local api = vim.api
+
+local opts = {
+  splitright = true,
+  splitbelow = true,
+  clipboard = 'unnamedplus',
+  hlsearch = true,
+  mouse = 'a',
+  whichwrap = 'b,s,h,l,<,>,[,]',
+  ignorecase = true,
+  smartcase = true,
+  pumheight = 10,
+  lazyredraw = true,
+  showcmd = false,
+  guicursor = vim.o.guicursor..',a:blinkon0',
+  encoding = 'utf-8',
+  undodir = vim.env.HOME..'/.local/share/nvim/backup',
+  undofile = true
+}
+
+local wopts = {
+  cursorline = true,
+  signcolumn = 'yes',
+  number = true,
+  foldmethod = 'marker'
+}
+
+local bopts = {
+  autoindent = true,
+  smartindent = true,
+  tabstop = 2,
+  shiftwidth = 2,
+  expandtab = true
+}
+
+for opt, val in pairs(opts) do
+  vim.api.nvim_set_option(opt, val)
+end
+
+for opt, val in pairs(wopts) do
+  vim.api.nvim_win_set_option(0, opt, val)
+end
+
+for opt, val in pairs(bopts) do
+  vim.api.nvim_buf_set_option(0, opt, val)
+end
