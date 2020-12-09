e3fe2b3c6a0af90c313ea0c8be287160a563ddea
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2020-12-10 08:47:40 +0900

    fix buflocal options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,47 +1,53 @@
-local api = vim.api
+function SetOptions()
+  local api = vim.api
 
-local opts = {
-  splitright = true,
-  splitbelow = true,
-  clipboard = 'unnamedplus',
-  hlsearch = true,
-  mouse = 'a',
-  whichwrap = 'b,s,h,l,<,>,[,]',
-  ignorecase = true,
-  smartcase = true,
-  pumheight = 10,
-  lazyredraw = true,
-  showcmd = false,
-  guicursor = vim.o.guicursor..',a:blinkon0',
-  encoding = 'utf-8',
-  undodir = vim.env.HOME..'/.local/share/nvim/backup',
-  termguicolors = true
-}
+  local opts = {
+    splitright = true,
+    splitbelow = true,
+    clipboard = 'unnamedplus',
+    hlsearch = true,
+    mouse = 'a',
+    whichwrap = 'b,s,h,l,<,>,[,]',
+    ignorecase = true,
+    smartcase = true,
+    pumheight = 10,
+    lazyredraw = true,
+    showcmd = false,
+    guicursor = vim.o.guicursor..',a:blinkon0',
+    encoding = 'utf-8',
+    undodir = vim.env.HOME..'/.local/share/nvim/backup',
+    termguicolors = true
+  }
 
-local wopts = {
-  cursorline = true,
-  signcolumn = 'yes',
-  number = true,
-  foldmethod = 'marker'
-}
+  local wopts = {
+    cursorline = true,
+    signcolumn = 'yes',
+    number = true,
+    foldmethod = 'marker'
+  }
 
-local bopts = {
-  autoindent = true,
-  smartindent = true,
-  tabstop = 2,
-  shiftwidth = 2,
-  expandtab = true,
-  undofile = true
-}
+  local bopts = {
+    autoindent = true,
+    smartindent = true,
+    tabstop = 2,
+    shiftwidth = 2,
+    expandtab = true,
+    undofile = true
+  }
 
-for opt, val in pairs(opts) do
-  vim.api.nvim_set_option(opt, val)
+  for opt, val in pairs(opts) do
+    vim.api.nvim_set_option(opt, val)
+  end
+
+  for opt, val in pairs(wopts) do
+    vim.api.nvim_win_set_option(0, opt, val)
+  end
+
+  for opt, val in pairs(bopts) do
+    vim.api.nvim_buf_set_option(0, opt, val)
+  end
 end
 
-for opt, val in pairs(wopts) do
-  vim.api.nvim_win_set_option(0, opt, val)
-end
+SetOptions()
 
-for opt, val in pairs(bopts) do
-  vim.api.nvim_buf_set_option(0, opt, val)
-end
+vim.cmd('autocmd BufEnter * lua SetOptions()')
